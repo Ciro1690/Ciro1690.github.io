b5fd35ddb51de7cb8a381a8260a23f31b2af0f30
Ciro <Ciro16@gmail.com>
2021-01-23 22:23:02 -0800

    changed website to use flask

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
+from forms import ContactForm
+from secrets import SECRET_KEY
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "abc123"
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'Ciro16@gmail.com'  # enter your email here
+app.config['MAIL_PASSWORD'] = SECRET_KEY # enter your password here
+
+mail = Mail(app)
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/recent_work")
+def recent_work():
+    return render_template("recent_work.html")
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
+
+@app.route("/reach_out", methods=['GET', 'POST'])
+def reach_out():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        msg = Message(f"Message from {name}", sender=f"{email}", recipients=['Ciro16@gmail.com'])
+        msg.body = f"{message} from {email}"
+
+        mail.send(msg)
+        flash("Email sent", "success")
+        return redirect('/')
+    return render_template("reach_out.html", form=form)
+
---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired
+
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired()])
+    message = StringField("Message", validators=[InputRequired()])
